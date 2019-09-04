6c8fc78941b2e6ee15013f41bfd52d5ff8c8d51a
Sean Boisselle <augmentedsean@gmail.com>
2019-09-04 00:50:47 -0400

    added sign in with google feature and fixed buttons

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -55,12 +55,14 @@ class SignIn extends React.Component {
                       />  
 
                  </form>
+                <div className='buttons'>
+                    <CustomButton type="submit"> Sign in</CustomButton>
+                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                        {' '}
+                        Sign in with Google{' '}
+                    </CustomButton>
+                </div>
 
-                 <CustomButton type="submit"> Sign in</CustomButton>
-                 <CustomButton onClick={signInWithGoogle}>
-                    {' '}
-                    Sign in with Google{' '}
-                 </CustomButton>
             
             </div>
         )

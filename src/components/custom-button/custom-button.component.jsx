6fc091ff18cd0c737b0d839430f74fcee970eac6
Sean Boisselle <augmentedsean@gmail.com>
2019-09-03 23:31:07 -0400

    fixed bug with firebase

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -6,4 +6,6 @@ const CustomButton = ({ children, ...otherProps} ) => (
     <button className='custom-button' {...otherProps}>
         { children }
     </button>
-)
+);
+
+export default CustomButton;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -57,7 +57,7 @@ class SignIn extends React.Component {
                  </form>
 
                  <CustomButton type="submit"> Sign in</CustomButton>
-                 <CustomButton onClick={signInWIthGoogle}>
+                 <CustomButton onClick={signInWithGoogle}>
                     {' '}
                     Sign in with Google{' '}
                  </CustomButton>

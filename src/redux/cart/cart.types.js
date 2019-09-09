43b46e1ecfa2d9daf8f8e5a2af3ba7090a7f8731
Sean Boisselle <augmentedsean@gmail.com>
2019-09-09 01:10:31 -0400

    updated shop data with new data, updated cart utils to group items in cart reducer.

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -4,7 +4,8 @@
 
 // export default CartActionTypes;
 const CartActionTypes = {
-    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
+    TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+    ADD_ITEM: 'ADD_ITEM'
 };
 
 export default CartActionTypes;

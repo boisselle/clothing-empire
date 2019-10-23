21045c2ef8ed29032466c978b6f71c470f0b8527
Sean Boisselle <augmentedsean@gmail.com>
2019-10-23 00:33:41 -0500

    fixed bug with collections component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -12,7 +12,7 @@ const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
             <img src='imageUrl' alt='item' />
           </div>
 
-          <span className='name'>{name}</span>
+          <span className='name'>{name}1</span>
           <span className='quantity'>
           <div className='arrow' onClick={() => removeItem(cartItem)}>&#10094;</div>
           <span className='value'>{quantity}</span>
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -6,7 +6,7 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 import { selectCollection } from '../../redux/shop/shop.selectors';
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => {
+const CollectionPage = ({ collection }) => { 
     const { title, items } = collection;
     return (
     <div className='collection-page'>
